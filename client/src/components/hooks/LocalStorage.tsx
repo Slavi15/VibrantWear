80d9b08fbfcd84763553c2f8253de76c4bea5812
Slavi15 <slavirezashkiofficial@gmail.com>
2024-07-19 18:23:57 +0300

    Fix custom hook

--- client/src/components/hooks/LocalStorage.tsx
+++ client/src/components/hooks/LocalStorage.tsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const useLocalStorage = (key : string, value : string) => {
+const useLocalStorage = (key: string, value: string) => {
     const [state, setState] = useState(() => {
+        if (typeof window === "undefined") {
+            return value; // Return initial value on server side
+        }
+
         try {
             const value = window.localStorage.getItem(key);
             return value ? JSON.parse(value) : value;
@@ -10,7 +14,17 @@ const useLocalStorage = (key : string, value : string) => {
         }
     })
 
-    const setValue = (value : any) => {
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            try {
+                window.localStorage.setItem(key, JSON.stringify(state));
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }, [key, state]);
+
+    const setValue = (value: any) => {
         try {
             const valueToStore = value instanceof Function ? value(state) : value;
             window.localStorage.setItem(key, JSON.stringify(valueToStore));
@@ -20,7 +34,7 @@ const useLocalStorage = (key : string, value : string) => {
         }
     }
 
-    return [state, setValue]
+    return [state, setValue];
 };
 
 export default useLocalStorage;
